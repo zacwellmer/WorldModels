c706a656f0d9f5a34bb0a0a4898e8a44575c748a
Josh Allen <jallen33@login.crane.hcc.unl.edu>
2022-03-29 10:14:21 -0500

    hcc_submit_gpu.sh

---
+++ hcc_submit_gpu.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#SBATCH --time=6:00:00          # Maximum run time in hh:mm:ss
+#SBATCH --nodes=1               # Run on one node
+#SBATCH --ntasks=9              # Number of tasks to run, 1CPU per task
+#SBATCH --mem=16000             # Maximum memory required (in megabytes)
+#SBATCH --job-name=default_479  # Job name (to track progress)
+#SBATCH --partition=cse479,cse479_preempt      # Partition on which to run job
+#SBATCH --gres=gpu:1            # Don't change this, it requests a GPU
+#SBATCH --error=job.%J.err
+#SBATCH --output=job.%J.out
+
+module load singularity
+singularity exec --pwd /home/ubuntu/WM/WorldModels/ -B $PWD:/home/ubuntu/WM/WorldModels/ docker://jallen33/wm bash ./launch_scripts/carracing.bash
